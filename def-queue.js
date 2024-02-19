3a90aecf0520f4a886532abc559b98ae0fa31e7c
urmi-js <149043645+urmi-js@users.noreply.github.com>
2024-02-19 19:41:22 +0530

    Add files via upload
    queue without operator

---
+++ def-queue.js
@@ -0,0 +1,13 @@
+class queue{
+    constroctor(){
+        this.item = [];
+    }
+    start(element){
+        this.item.push(element);
+    }
+    end(){
+        if(this.isempty() === false){
+
+        }
+    }
+}
