96ee6d6bc346cf92b4bc45e9c5d2ab1540b8f00e
urmi-js <149043645+urmi-js@users.noreply.github.com>
2024-02-24 11:34:23 +0530

    Update def-queue.js

--- def-queue.js
+++ def-queue.js
@@ -1,13 +1,45 @@
 class queue{
     constroctor(){
         this.item = [];
+        this.start = 0;
+        this.end = 0;
     }
-    start(element){
-        this.item.push(element);
+    start(item){
+        this.item[this.end] = item;
+        this.end++;
+        return item + 'inserted';
     }
     end(){
-        if(this.isempty() === false){
-
-        }
+        const item = this.item[this.start];
+        delete this.item[this.start];
+        this.start++;
+        return item + 'deleted';
+    }
+    isEmpty(){
+        return this.start === this.end;
+    }
+    peek(){
+        return this.item[this.start];
+    }
+    printQueue(){
+        return this.item;
     }
 }
+
+Q1 = new queue();
+console.log(q1.enqueue(10));
+console.log(q1.enqueue(20));
+console.log(q1.enqueue(30));
+console.log(q1.enqueue(40));
+console.log(q1.enqueue(50));
+console.log("After pushing 5 elements");
+console.log(q1.printQueue());
+console.log(q1.dequeue());
+console.log(q1.dequeue());
+console.log("After popping 2 elements");
+console.log(q1.printQueue());
+console.log(q1.enqueue(60));
+console.log(q1.printQueue());
+console.log("Front element");
+console.log(q1.peek());
+console.log(q1.isEmpty());
