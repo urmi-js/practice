27f0a3402f26e47fcc123e152dcfdf46550d0eca
urmi-js <149043645+urmi-js@users.noreply.github.com>
2024-03-01 20:13:13 +0530

    Add files via upload

---
+++ single-LL.js
@@ -0,0 +1,21 @@
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
+      this.size = 0;
+    }
+
+ add(data){
+    let Node = new Node(data);
+     if{
+
+     }else{
+     }
+    }
+  }
