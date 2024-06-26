15cede10ce8904763f47e4de05b11153679758dc
urmi-js <149043645+urmi-js@users.noreply.github.com>
2024-06-26 12:23:38 +0530

    Add files via upload
    count number of same charcter in string

---
+++ char.js
@@ -0,0 +1,11 @@
+let string = "aaaba";
+let value = "a";
+let charcter = 0;
+let 
+
+for(i= 0; i<string.length; i++){
+    if(string[i]=== value)
+        charcter++;
+}
+console.log("charcter no:" ,charcter );
+
